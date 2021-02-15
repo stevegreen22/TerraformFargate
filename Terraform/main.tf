ec50ee3cb26a976fab174f4e279f3ce4b2056ac6
Steve <Stevegreen22@me.com>
2021-02-15 10:42:07 +0000

    Adding ECS service, some issues with taskdef template

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -109,7 +109,7 @@ resource "aws_lb_listener" "https" {
 
 #ecr repo
 resource "aws_ecr_repository" "repo" {
-  name = "ecr_repo"
+  name = "hellolinius"
 }
 
 #IAM role for executing the tasks
@@ -135,4 +135,122 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# pull in the task def from the external file
+data "template_file" "hello-app" {
+  #template = "./hallo-app-taskdef.json"
+  template = "./test.json.tpl"
+  #template = file("./test.json.tpl")
+  vars = {
+    aws_ecr_repository = aws_ecr_repository.repo.repository_url
+    app_port           = 80
+    tag                = "latest"
+  }
+}
+
+# create the service task def
+resource "aws_ecs_task_definition" "app_service_td" {
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  #container_definitions    = file("test.json")
+  #container_definitions    = data.template_file.hello-app.rendered
+  container_definitions = <<TASK_DEFINITION
+  [
+    {
+      "name": "hello-app",
+      "image": "322244822111.dkr.ecr.us-east-1.amazonaws.com/hellolinius:py0.1",
+      "essential": true,
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-region": "us-east-1",
+          "awslogs-stream-prefix": "helloapp-service-stream",
+          "awslogs-group": "helloapp-loggroup"
+        }
+      },
+      "portMappings": [
+        {
+          "containerPort": 4000,
+          "hostPort": 4000,
+          "protocol": "tcp"
+        }
+      ],
+      "cpu": 1,
+      "environment": [
+        {
+          "name": "NODE_ENV",
+          "value": "test"
+        },
+        {
+          "name": "PORT",
+          "value": "4000"
+        }
+      ],
+      "ulimits": [
+        {
+          "name": "nofile",
+          "softLimit": 65536,
+          "hardLimit": 65536
+        }
+      ],
+      "mountPoints": [],
+      "memory": 2048,
+      "volumesFrom": []
+    }
+  ]
+  TASK_DEFINITION
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  family                   = "hello-app-test"
+  memory                   = 2048
+  cpu                      = 256
+  tags = {
+    Environment = "test"
+  }
+}
+
+resource "aws_ecs_cluster" "app-cluster" {
+  name = "helloapp-cluster"
+}
+
+# the service
+resource "aws_ecs_service" "app-service" {
+  name            = "app-test-service"
+  cluster         = aws_ecs_cluster.app-cluster.id
+  task_definition = aws_ecs_task_definition.app_service_td.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups  = [aws_security_group.ecs_tasks_sg.id]
+    subnets          = data.aws_subnet_ids.default.ids
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = "hello-app"
+    container_port   = 4000
+  }
+
+  depends_on = [
+    aws_lb_listener.https,
+    aws_iam_role_policy_attachment.ecs_task_execution_role
+  ]
+
+  tags = {
+    Environment = "test"
+  }
+}
+
+# to prevent container from failing on startup - assuming it starts up
+# "awslogs-region": "us-east-1",
+# "awslogs-stream-prefix": "helloapp-service-stream",
+# "awslogs-group": "helloapp-loggroup"
+resource "aws_cloudwatch_log_group" "helloapp-loggroup" {
+  name = "helloapp-loggroup"
+  #todo: find a better way of having this present in all resources.
+  tags = {
+    Environment = "test"
+  }
 }
